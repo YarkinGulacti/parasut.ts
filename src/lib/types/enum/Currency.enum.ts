d5fbada7a42754be3f744c72930d4982e50a10e0
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2023-01-22 13:55:13 +0300

    all response fields made optional created enums. version upgrade.

---
+++ src/lib/types/enum/Currency.enum.ts
@@ -0,0 +1,6 @@
+export enum Currency {
+    TRL = "TRL",
+    USD = "USD",
+    EUR = "EUR",
+    GBP = "GBP",
+}
---
+++ src/lib/types/enum/ResourceType.enum.ts
@@ -0,0 +1,13 @@
+export enum ResourceType {
+    SalesInvoices = "sales_invoices",
+    ItemCategories = "item_categories",
+    Contacts = "contacts",
+    SalesInvoiceDetails = "sales_invoice_details",
+    Payments = "payments",
+    Tags = "tags",
+    SalesOffers = "sales_offers",
+    Sharings = "sharings",
+    RecurrencePlans = "recurrence_plans",
+    EArchives = "e_archives",
+    EInvoices = "e_invoices",
+}
