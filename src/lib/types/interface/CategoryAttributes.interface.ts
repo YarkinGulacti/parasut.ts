d51a5812b7c0f8c626c2811592298737fa59e497
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2023-02-13 12:39:56 +0300

    made fields optional.

--- src/lib/types/interface/CategoryAttributes.interface.ts
+++ src/lib/types/interface/CategoryAttributes.interface.ts
@@ -1,12 +1,12 @@
 import { CategoryType } from "../enum";
 
 export interface CategoryAttributes {
-    full_path: Array<string>;
-    created_at: string;
-    updated_at: string;
-    name: string;
-    bg_color: string;
-    text_color: string;
-    category_type: CategoryType;
-    parent_id: number;
+    full_path?: Array<string>;
+    created_at?: string;
+    updated_at?: string;
+    name?: string;
+    bg_color?: string;
+    text_color?: string;
+    category_type?: CategoryType;
+    parent_id?: number;
 }
